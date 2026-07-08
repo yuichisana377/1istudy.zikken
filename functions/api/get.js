f97c73fd226af91d76a42ce2ea8493da91f65062
yuichisana377 <yuichi_sana3770@icloud.com>
2026-07-08 22:50:49 +0900

    Update get.js

--- functions/api/get.js
+++ functions/api/get.js
@@ -1,1 +1,6 @@
 
+export async function onRequest({ env }) {
+  const id = env.details.idFromName("main");
+  const obj = env.details.get(id);
+  return obj.fetch(new Request("http://dummy/get"));
+}
