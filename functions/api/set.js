409ac1401f5b43d470e259e927279a02117897a7
yuichisana377 <yuichi_sana3770@icloud.com>
2026-07-08 22:50:59 +0900

    Update set.js

--- functions/api/set.js
+++ functions/api/set.js
@@ -1,1 +1,9 @@
+export async function onRequest({ env, request }) {
+  const id = env.details.idFromName("main");
+  const obj = env.details.get(id);
+  return obj.fetch(new Request("http://dummy/set", {
+    method: "POST",
+    body: await request.text()
+  }));
+}
 
