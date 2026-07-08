0d5086a3518e4bd2b56673d62955c9a9a975226a
yuichisana377 <yuichi_sana3770@icloud.com>
2026-07-08 22:50:33 +0900

    Update details.js

--- functions/details.js
+++ functions/details.js
@@ -1,1 +1,25 @@
 
+export class details {
+  constructor(state) {
+    this.state = state;
+  }
+
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    if (url.pathname.endsWith("/get")) {
+      const data = await this.state.storage.get("data");
+      return new Response(JSON.stringify(data || {}), {
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    if (url.pathname.endsWith("/set")) {
+      const body = await request.json();
+      await this.state.storage.put("data", body);
+      return new Response("OK");
+    }
+
+    return new Response("Not found", { status: 404 });
+  }
+}
